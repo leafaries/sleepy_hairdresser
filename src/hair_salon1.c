e25d0851dbc6367a9a2922c7b1783d0fc4e98797
Maciej Konopka <leafaries@proton.me>
2024-06-04 22:30:17 +0200

    pretty sure both parts work

--- src/hair_salon1.c
+++ src/hair_salon1.c
@@ -1,5 +1,6 @@
-/*#include <pthread.h>
+#include <pthread.h>
 #include <semaphore.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,175 +14,146 @@
 #define NUM_CHAIRS 5
 #define TOTAL_CUSTOMERS 10
 
-void print_waiting_queue();
-void print_resigned_customers();
 void print_barbershop_status();
 unsigned int generate_initial_seed();
 unsigned int get_my_seed();
-void simulate_work(unsigned int *seed);
+void simulate_work(int max_seconds);
 void *barber_thread_routine(void *arg);
 void *customer_thread_routine(void *arg);
 void *customer_generator_thread_routine(void *arg);
 
 sem_t barber_ready;
+sem_t haircut_done;
 sem_t customer_ready;
-pthread_mutex_t seats_mutex;
-pthread_mutex_t barberchair_mutex;
-pthread_mutex_t resigned_customer_counter_mutex;
-pthread_mutex_t queue_mutex;
+pthread_mutex_t mutex;
 
 int available_seats = NUM_CHAIRS;
 int current_customer_on_barberchair = 0;
 Queue *customer_queue;
 int resigned_customers[TOTAL_CUSTOMERS];
 int resigned_customers_counter = 0;
-int print_info = 0;
+bool print_info = false;
 
 __thread unsigned int thread_seed = 0;
+
+/**
+ * Initializes resources used in the barbershop simulation, such as mutexes and condition variables.
+ */
+void initialize_resources();
+
+/**
+ * Destroys resources used in the barbershop simulation to prevent resource leaks.
+ */
+void destroy_resources();
 
 int main(int argc, char *argv[])
 {
     if (argc > 1 && strcmp(argv[1], "-info") == 0)
     {
-        print_info = 1;
+        print_info = true;
     }
+
+    initialize_resources();
 
     pthread_t barber_thread;
     pthread_t customer_generator_thread;
 
-    // Initialize semaphores
-    sem_init(&barber_ready, 0, 0);
-    sem_init(&customer_ready, 0, 0);
-
-    // Initialize mutex
-    pthread_mutex_init(&seats_mutex, NULL);
-    pthread_mutex_init(&barberchair_mutex, NULL);
-    pthread_mutex_init(&resigned_customer_counter_mutex, NULL);
-    pthread_mutex_init(&queue_mutex, NULL);
-
-    // Create the customer queue
-    customer_queue = create_queue();
-
-    // Create threads
     pthread_create(&barber_thread, NULL, barber_thread_routine, NULL);
     pthread_create(&customer_generator_thread, NULL, customer_generator_thread_routine, NULL);
 
-    // Wait for threads to finish
     pthread_join(barber_thread, NULL);
     pthread_join(customer_generator_thread, NULL);
 
-    // Clean up resources
-    sem_destroy(&barber_ready);
-    sem_destroy(&customer_ready);
-
-    pthread_mutex_destroy(&seats_mutex);
-    pthread_mutex_destroy(&barberchair_mutex);
-    pthread_mutex_destroy(&resigned_customer_counter_mutex);
-    pthread_mutex_destroy(&queue_mutex);
+    destroy_resources();
 
     return 0;
 }
 
-void print_waiting_queue()
+void initialize_resources()
 {
-    pthread_mutex_lock(&queue_mutex);
-    printf("Aktualna kolejka oczekujących klientów: ");
-    Node *current = customer_queue->front;
-    while (current != NULL)
+    // Initialize mutex
+    if (pthread_mutex_init(&mutex, NULL) != 0)
     {
-        printf("%d ", current->data);
-        current = current->next;
+        perror("Failed to initialize mutex");
+        exit(EXIT_FAILURE);
     }
-    printf("\n");
-    pthread_mutex_unlock(&queue_mutex);
+
+    if (sem_init(&barber_ready, 0, 0) != 0 ||
+        sem_init(&haircut_done, 0, 0) != 0 ||
+        sem_init(&customer_ready, 0, 0) != 0)
+    {
+        perror("Failed to initialize a semaphore");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create the customer queue
+    customer_queue = create_queue();
+    if (customer_queue == NULL)
+    {
+        perror("Failed to create customer queue");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void print_resigned_customers()
+void destroy_resources()
 {
-    pthread_mutex_lock(&resigned_customer_counter_mutex);
-    printf("Klienci którzy nie dostali się do gabinetu: ");
-    for (int i = 0; i < resigned_customers_counter; i++)
-    {
-        printf("%d ", resigned_customers[i]);
-    }
-    printf("\n");
-    pthread_mutex_unlock(&resigned_customer_counter_mutex);
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&barber_ready);
+    sem_destroy(&haircut_done);
+    sem_destroy(&customer_ready);
+    destroy_queue(customer_queue);
 }
 
 void print_barbershop_status()
 {
-    pthread_mutex_lock(&resigned_customer_counter_mutex);
-    pthread_mutex_lock(&seats_mutex);
-    pthread_mutex_lock(&barberchair_mutex);
     printf("Rezygnacja: %d Poczekalnia: %d/%d [Fotel: %d]\n", resigned_customers_counter,
                                                               NUM_CHAIRS - available_seats,
                                                               NUM_CHAIRS,
                                                               current_customer_on_barberchair);
-    pthread_mutex_unlock(&resigned_customer_counter_mutex);
-    pthread_mutex_unlock(&seats_mutex);
-    pthread_mutex_unlock(&barberchair_mutex);
 }
 
-unsigned int generate_initial_seed()
+void simulate_work(int max_seconds)
 {
-    // Using time and process ID as a simple seed generator method
-    return (unsigned int)time(NULL) ^ (unsigned int)getpid();
-}
-
-unsigned int get_my_seed()
-{
-    // Check if the seed has been initialized
-    if (thread_seed == 0)
-    {
-        thread_seed = generate_initial_seed();
-    }
-    return thread_seed;
-}
-
-void simulate_work(unsigned int *seed)
-{
-    int time = rand_r(seed) % 5000000; // [0; 5 seconds)
-    usleep(time);
-}
-
-void simulate_short_work(unsigned int *seed)
-{
-    int time = rand_r(seed) % 1000000; // [0; 1 second)
-    usleep(time);
+    unsigned int seed = (thread_seed == 0) ? (unsigned int)time(NULL) ^ (unsigned int)getpid() : thread_seed;
+    usleep(rand_r(&seed) % (max_seconds * 1000000));
 }
 
 void *barber_thread_routine(void *arg)
 {
     while (1)
     {
+
         sem_wait(&customer_ready);
 
-        pthread_mutex_lock(&seats_mutex);
+        pthread_mutex_lock(&mutex);
+        // Double-check to ensure the queue isn't empty before proceeding
+        if (queue_is_empty(customer_queue)) {
+            pthread_mutex_unlock(&mutex);
+            continue; // Go back to waiting if no customer is actually ready
+        }
+        current_customer_on_barberchair = dequeue(customer_queue);
         available_seats++;
-        pthread_mutex_unlock(&seats_mutex);
         print_barbershop_status();
-
-        pthread_mutex_lock(&queue_mutex);
-        int customer_id = dequeue(customer_queue);
-        pthread_mutex_unlock(&queue_mutex);
         if (print_info)
         {
-            print_waiting_queue();
+            print_waiting_queue(customer_queue);
         }
-
+        pthread_mutex_unlock(&mutex);
         sem_post(&barber_ready);
 
-        pthread_mutex_lock(&barberchair_mutex);
-        current_customer_on_barberchair = customer_id;
-        pthread_mutex_unlock(&barberchair_mutex);
-        print_barbershop_status();
-        unsigned int my_seed = get_my_seed();
-        simulate_work(&my_seed);
+        simulate_work(5);
 
-        pthread_mutex_lock(&barberchair_mutex);
+        pthread_mutex_lock(&mutex);
         current_customer_on_barberchair = 0;
-        pthread_mutex_unlock(&barberchair_mutex);
         print_barbershop_status();
+        if (print_info)
+        {
+            print_waiting_queue(customer_queue);
+        }
+        pthread_mutex_unlock(&mutex);
+
+        // Signal that the haircut is done
+        sem_post(&haircut_done);
     }
 
     return NULL;
@@ -191,37 +163,36 @@ void *customer_thread_routine(void *arg)
 {
     int id = *(int *)arg;
     free(arg);
-    pthread_mutex_lock(&seats_mutex);
 
-    if (available_seats > 0)
-    {
-        available_seats--;
-        pthread_mutex_unlock(&seats_mutex);
-        print_barbershop_status();
+    pthread_mutex_lock(&mutex);
 
-        pthread_mutex_lock(&queue_mutex);
-        enqueue(customer_queue, id);
-        pthread_mutex_unlock(&queue_mutex);
-        if (print_info)
-        {
-            print_waiting_queue();
-        }
-
-        sem_post(&customer_ready);
-        sem_wait(&barber_ready);
-    }
-    else
-    {
-        pthread_mutex_unlock(&seats_mutex);
-        pthread_mutex_lock(&resigned_customer_counter_mutex);
+    // Check if the waiting room is full
+    if (available_seats == 0) {
         resigned_customers[resigned_customers_counter++] = id;
-        pthread_mutex_unlock(&resigned_customer_counter_mutex);
-        print_barbershop_status();
-        if (print_info)
-        {
-            print_resigned_customers();
-        }
+        print_resigned_customers(resigned_customers_counter, resigned_customers);
+        pthread_mutex_unlock(&mutex);
+        return NULL;
     }
+
+    // Enqueue the customer since there are available seats
+    enqueue(customer_queue, id);
+    available_seats--;
+    print_barbershop_status();
+    if (print_info) {
+        print_waiting_queue(customer_queue);
+    }
+
+    // Signal the barber that a customer is ready
+    sem_post(&customer_ready);
+
+    pthread_mutex_unlock(&mutex);
+
+    // Wait until the barber is ready for this customer
+    sem_wait(&barber_ready);
+
+    // Wait for the haircut to complete
+    sem_wait(&haircut_done);
+
     return NULL;
 }
 
@@ -246,8 +217,7 @@ void *customer_generator_thread_routine(void *arg)
             exit(EXIT_FAILURE);
         }
 
-        unsigned int my_seed = get_my_seed();
-        simulate_short_work(&my_seed);
+        simulate_work(1);
     }
 
     for (int i = 0; i < TOTAL_CUSTOMERS; i++)
@@ -257,9 +227,4 @@ void *customer_generator_thread_routine(void *arg)
 
     return NULL;
 }
-*/
 
-int main()
-{
-    return 0;
-}
